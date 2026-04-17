f6681e4316732fb1fe0196a4cd900292999e2802
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2026-04-17 01:31:06 -0700

    Add descriptions to cardData

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const cardData = {
     'DPR IAN Mito': {
         'title': 'DPR IAN Mito',
         'timestamp': '2026-02-16T06:07:44.996Z',
+        'description': 'A monochrome portrait of the artist DPR IAN as his character MITO.',
         'id': 'dpr_ian_mito',
         'src': 'dpr_ian_mito.png',
         'likes': 54,
@@ -20,6 +21,7 @@ const cardData = {
     'DPR IAN': {
         'title': 'DPR IAN',
         'timestamp': '2025-11-01T07:22:10.996Z',
+        'description': 'A portrait of my favorite artist, DPR IAN, in an outfit for his SAINT album.',
         'id': 'dpr_ian',
         'src': 'dpr_ian.png',
         'likes': 300,
@@ -31,6 +33,7 @@ const cardData = {
     'Halloween GBC Sprites': {
         'title': 'Halloween GBC Sprites',
         'timestamp': '2025-06-23T07:59:10.996Z',
+        'description': 'Sprites and portraits of little Halloween characters.',
         'id': 'halloween_gbc_sprites',
         'src': 'halloween_gbc_sprites.png',
         'likes': 54,
@@ -42,6 +45,7 @@ const cardData = {
     'V': {
         'title': 'V',
         'timestamp': '2025-06-03T02:17:47.996Z',
+        'description': 'V from BTS as he appeared on a Vogue Korea magazine cover.',
         'id': 'v',
         'src': 'v.png',
         'likes': 81,
@@ -53,6 +57,7 @@ const cardData = {
     'Hands Study': {
         'title': 'Hands Study',
         'timestamp': '2024-11-22T16:25:39.996Z',
+        'description': 'I wanted to get better at drawing hands, so I drew a bunch of them in different angles.',
         'id': 'hands_study',
         'src': 'hands_study.png',
         'likes': 662,
@@ -64,6 +69,7 @@ const cardData = {
     'May': {
         'title': 'May',
         'timestamp': '2024-04-14T00:37:56.996Z',
+        'description': 'May from Pokémon Ruby/Sapphire, drawn with my 4-color Red Bean Paste palette.',
         'id': 'may',
         'src': 'may.png',
         'likes': 67,
@@ -75,6 +81,7 @@ const cardData = {
     'clockworkgrn Portraits': {
         'title': 'clockworkgrn Portraits',
         'timestamp': '2024-07-24T07:08:26.996Z',
+        'description': "I drew my friend clockworkgrn's original characters for Art Fight 2024!",
         'id': 'clockworkgrn_portraits',
         'src': 'clockworkgrn_portraits.png',
         'likes': 63,
@@ -86,6 +93,7 @@ const cardData = {
     'Advance Wars!': {
         'title': 'Advance Wars!',
         'timestamp': '2023-08-29T04:41:09.996Z',
+        'description': 'Nine 32x32 portraits of characters from Advance Wars, drawn with my 4-color Red Bean Paste palette.',
         'id': 'advance',
         'src': 'advance.png',
         'likes': 197,
@@ -97,6 +105,7 @@ const cardData = {
     'Pokémon Ranger': {
         'title': 'Pokémon Ranger',
         'timestamp': '2023-06-23T01:06:18.996Z',
+        'description': 'A 56x56 sprite of a Pokémon ranger in the style of Gameboy Color games.',
         'id': 'ranger',
         'src': 'ranger.png',
         'likes': 35,
